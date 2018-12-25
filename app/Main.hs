8a79c4fb4e628f5befe99000f7ca4f5808699665
Marei Kikukawa <contact@calmery.me>
2018-12-25 16:35:41 +0900

    コマンドライン引数からファイルを指定できるように変更する

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
-import           Parser (parse)
+import           Parser             (parse)
+import           System.Environment (getArgs)
+import           System.IO          (IOMode (ReadMode), hClose, hGetContents,
+                                     openFile)
 
 main :: IO ()
-main = loop
-  where
-    loop = do
-      input <- getLine
-      putStrLn $ parse input
-      loop
+main = do
+  args <- getArgs
+  if 1 <= length args then do
+    handle <- openFile (head args) ReadMode
+    contents <- hGetContents handle
+    putStrLn $ parse contents
+    hClose handle
+  else
+    putStrLn "Argument not specified"
